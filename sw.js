a0bfd565c888bd6c261a8ce22cf9a14ef683b6b6
Rados Rajcic <RajcicRados@hotmail.com>
2024-06-02 18:59:55 +0200

    modified:   index.html 	renamed:    site.webmanifest -> site.webmanifest.json 	new file:   sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+// On install - the application shell cached
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("sw-cache").then(function (cache) {
+      // Static files that make up the application shell are stored in cache
+      return cache.add([
+        "/index.html",
+        "/assets/css/risen.css",
+        "/assets/fontawesome/js/all.js",
+        "/js/fontawesome.js",
+        "/assets/fontawesome/css/all.css",
+        "/assets/fontawesome/js/all.js",
+        "/assets/images/favicon.png",
+      ]);
+    })
+  );
+});
+
+// with requested network
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // return it if there is a response, or else fetch it again
+      return response || fetch(event.request);
+    })
+  );
+});
